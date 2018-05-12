ae7dabf80f660f99980b02a8e0306bf6fc89aa5f
keisuke taniguchi <rememberthatk@gmail.com>
2018-05-12 11:16:02 +0000

    Add vocab_list sample;

--- vocab_list.rb
+++ vocab_list.rb
@@ -1,11 +1,38 @@
 # frozen_string_literal: true
 
+# POINT: FOLLOW THIS PATTERN
+#
+# remember 'happy' do
+#   @answer = '幸せ'
+#   @wrong1 = '悲しい'
+#   @wrong2 = '料理する'
+#   @wrong3 = '大変な'
+# end
+
 remember 'eliminate' do
-  answer '除外する'
-  wrongs %w[加える 料理する 飛ぶ]
+  @answer = '除外する'
+  @wrong1 = '加える'
+  @wrong2 = '料理する'
+  @wrong3 = '飛ぶ'
 end
 
 remember 'justify' do
-  answer '正当化する'
-  wrongs %w[間違う 調節する 困る]
+  @answer = '正当化する'
+  @wrong1 = '間違う'
+  @wrong2 = '調節する'
+  @wrong3 = '困る'
+end
+
+remember 'flutter' do
+  @answer = 'お世辞を言う'
+  @wrong1 = '間違う'
+  @wrong2 = '笑う'
+  @wrong3 = '困る'
+end
+
+remember 'happy' do
+  @answer = '幸せ'
+  @wrong1 = '悲しい'
+  @wrong2 = '料理する'
+  @wrong3 = '大変な'
 end
